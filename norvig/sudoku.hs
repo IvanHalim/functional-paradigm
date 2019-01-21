50145ab21505db9b73a084d1fcb1c945410a6159
ivanhalim <ivanhalim@users.noreply.github.com>
2019-01-20 21:32:26 -0800

    Update sudoku.hs

--- norvig/sudoku.hs
+++ norvig/sudoku.hs
@@ -1,18 +1,21 @@
 module Main where
 
+import Data.Map (Map)
+import qualified Data.Map as Map
 import Data.List
-import Data.Array
+import Data.Maybe
 
-type Square = (Char, Char)
+type Square = String
 type Unit   = [Square]
+type Digit  = Char
+type Grid   = Map Square [Digit]
 
 cross :: String -> String -> [Square]
-cross rows cols = [(r,c) | r <- rows, c <- cols]
+cross rows cols = [r:c:[] | r <- rows, c <- cols]
 
 digits  = "123456789"
 rows    = "ABCDEFGHI"
 cols    = digits
-box     = (('A','1'), ('I','9'))
 
 squares :: [Square]
 squares = cross rows cols
@@ -23,12 +26,27 @@ unitlist = [cross rows [c]  | c  <- cols] ++
            [cross rs   cs   | rs <- ["ABC","DEF","GHI"],
                               cs <- ["123","456","789"]]
 
-units :: Array Square [Unit]
-units = array box [(s, [filter (/= s) u | u <- unitlist, elem s u]) | s <- squares]
+units :: Map Square [Unit]
+units = Map.fromList [(s, [filter (/= s) u | u <- unitlist, elem s u]) | s <- squares]
 
-peers :: Array Square [Square]
-peers = array box [(s, set(units!s)) | s <- squares]
-    where set = nub . concat
+set :: Eq a => Maybe [[a]] -> [a]
+set Nothing  = []
+set (Just x) = nub (concat x)
+
+peers :: Map Square [Square]
+peers = Map.fromList [(s, set (Map.lookup s units)) | s <- squares]
+
+emptyGrid :: Grid
+emptyGrid = Map.fromList [(s, digits) | s <- squares]
+
+parse_grid :: String -> Maybe Grid
+parse_grid = undefined
+
+grid_values :: String -> Maybe ([(Square, Char)])
+grid_values grid =
+    let chars = [c | c <- grid, elem c "0.123456789"] in
+        if length chars /= 81 then Nothing
+        else Just (zip squares chars)
 
 main :: IO ()
 main = return ()
