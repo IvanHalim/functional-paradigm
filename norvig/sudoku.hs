39d346908e725ced58ff36093e5e7d785e841d6d
ivanhalim <ivanhalim@users.noreply.github.com>
2019-01-28 00:40:55 -0800

    Update sudoku.hs

--- norvig/sudoku.hs
+++ norvig/sudoku.hs
@@ -126,7 +126,7 @@ gridToString (Just g) = unlines l5
         line    = intercalate "+" (replicate 3 hyphens)
 
 display :: Maybe Grid -> IO ()
-display = putStrLn. gridToString
+display = putStrLn . gridToString
 
 grid1  = "003020600900305001001806400008102900700000008006708200002609500800203009005010300"
 grid2  = "4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......"
